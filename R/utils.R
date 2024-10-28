3cf4fe57d48f681cf9c6eea7f3a4f2636f6ffe4a
jenna-a2ai <jenna@a2-ai.com>
2024-10-28 18:39:23 +0000

    add default paths to ghqc_libpath and ghqc_infopath documentation

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,6 @@
-#' The default install location for the ghqc package and its dependency. If it does not exist, it will be created
+#' The default install location for the ghqc package and its dependencies. If it does not exist, it will be created.
 #'
-#' @return string containing the default lib path for the ghqc package and its dependencies
+#' @return string containing the default lib path for the ghqc package and its dependencies (~/.local/share/ghqc/rpkgs)
 #'
 #' @importFrom fs dir_exists
 #' @importFrom fs dir_create
@@ -14,7 +14,7 @@ ghqc_libpath <- function() {
 
 #' The default install location for the ghqc customizing information repository
 #'
-#' @return string containing the default path to the ghqc information repository
+#' @return string containing the default path to the ghqc information repository (~/.local/share/ghqc/&lt;info repo name here&gt;
 #' @export
 ghqc_infopath <- function() {
   file.path("~/.local/share/ghqc", info_repo_name())
