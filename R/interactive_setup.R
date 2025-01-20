2c38531c6f6e09fe5b2c8cbd8c9ff00b3a875d39
jenna-a2ai <jenna@a2-ai.com>
2025-01-20 18:00:10 +0000

    update ghqc_explicit_depends

--- R/interactive_setup.R
+++ R/interactive_setup.R
@@ -141,15 +141,37 @@ interactive_depends <- function() {
 interactive_install <- function() {
   use_pak <- TRUE
   if (!rlang::is_installed("pak")) {
-    cli::cli_inform(" ")
-    yN <- gsub('\"', "", readline("Package `pak` is not found in your project package library. \nTo improve performance, would you like to install pak? (y/N) "))
-    if (yN == "y" || yN == "") utils::install.packages("pak") else use_pak <- FALSE
-  }
+    repeat {
+      cli::cli_inform(" ")
+      cli::cli_inform("Package {.code pak} is not found in your project package library")
+      yN_pak <- gsub('\"', "", readline("To improve performance, would you like to install `pak`? (y/N) "))
+      if (yN_pak %in% c("y", "Y", "")) {
+        utils::install.packages("pak")
+        break
+      }
+      else if (yN_pak %in% c("n", "N")) {
+        cli::cli_inform("{.code pak} not installed")
+        use_pak <- FALSE
+        break
+      }
+      cli::cli_inform("Unrecognized input. Please input 'y' or 'N'")
+    } # repeat
+  } # if
 
-  cli::cli_inform(" ")
-  lib_path <- gsub('\"', "", readline(paste0("Path to install the ghqc.app dependencies (", ghqc_libpath(), ") ")))
-  if (lib_path == "") lib_path <- ghqc_libpath()
-  if (!fs::file_exists(lib_path)) fs::dir_create(lib_path)
+  repeat {
+    cli::cli_inform(" ")
+    yN_libpath <- gsub('\"', "", readline(glue::glue("Install ghqc.app dependencies to path: {ghqc_libpath()} (y/N) ")))
+    if (yN_libpath %in% c("y", "Y", "")) {
+      lib_path <- ghqc_libpath()
+      break
+    }
+    else if (yN_libpath %in% c("n", "N")) {
+      lib_path <- gsub('\"', "", readline(glue::glue("Provide path to install ghqc.app dependencies: ")))
+      if (!fs::file_exists(lib_path)) fs::dir_create(lib_path)
+      break
+    }
+    cli::cli_inform("Unrecognized input. Please input 'y' or 'N'")
+  } # repeat
 
   cli::cli_inform(" ")
   check_ghqcapp_dependencies(lib_path = lib_path, use_pak = use_pak)
@@ -163,19 +185,31 @@ interactive_link <- function() {
   cli::cli_inform(" ")
   link_path <- gsub('\"', "", readline("Path to previously installed package library from which to link: "))
 
-  tryCatch({
+  result <- tryCatch({
     check_link_path(link_path)
+    TRUE
   }, error = function(e) {
-    cli::cli_alert_danger("Path provided is not sufficient because {e$message}")
-    return(invisible())
+    cli::cli_alert_danger("Path provided is not sufficient: {conditionMessage(e)}")
+    FALSE
   })
 
-  yN <- gsub('\"', "", readline(paste0("Link ghqc.app dependencies to path: ", ghqc_libpath(), " (y/N) ")))
-  if (yN == "y" || yn == "Y") lib_path <- ghqc_libpath()
-  else if (yN == "n" || yN == "N") {
-    libpath <- gsub('\"', "", readline(paste0("Link ghqc.app dependencies to path: ", ghqc_libpath(), " (y/N) ")))
-    if (!fs::file_exists(lib_path)) fs::dir_create(lib_path)
+  if (!result) { # can't return from function in tryCatch
+    return(invisible())
   }
+
+  repeat {
+    yN_libpath <- gsub('\"', "", readline(glue::glue("Link ghqc.app dependencies to path: {ghqc_libpath()} (y/N) ")))
+    if (yN_libpath %in% c("y", "Y", "")) {
+      lib_path <- ghqc_libpath()
+      break
+    }
+    else if (yN_libpath %in% c("n", "N")) {
+      lib_path <- gsub('\"', "", readline(paste0("Provide path to link ghqc.app dependencies: ")))
+      if (!fs::file_exists(lib_path)) fs::dir_create(lib_path)
+      break
+    }
+    cli::cli_inform("Unrecognized input. Please input 'y' or 'N'")
+  } # repeat
 
   cli::cli_inform(" ")
   link_ghqcapp_dependencies(link_path = link_path, lib_path = lib_path)
